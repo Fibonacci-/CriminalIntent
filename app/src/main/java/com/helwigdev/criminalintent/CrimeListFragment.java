fa3546227409fefbc34b98bb2343bf43a784e70f
helwig <helwig@susqu.edu>
2015-02-04 18:08:33 -0500

    2.4.14 further book progress working on challenge 16 p 270

--- app/src/main/java/com/helwigdev/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/helwigdev/criminalintent/CrimeListFragment.java
@@ -1,17 +1,24 @@
 package com.helwigdev.criminalintent;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.support.v7.app.ActionBar;
 
 import java.text.DateFormat;
 import java.util.ArrayList;
@@ -21,6 +28,7 @@ import java.util.ArrayList;
  */
 public class CrimeListFragment extends ListFragment {
 	private ArrayList<Crime> mCrimes;
+	private boolean mSubtitleVisible;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -29,10 +37,25 @@ public class CrimeListFragment extends ListFragment {
 		mCrimes = CrimeLab.get(getActivity()).getCrimes();
 
 		setHasOptionsMenu(true);
-
+		setRetainInstance(true);
+		mSubtitleVisible = false;
 		CrimeAdapter adapter = new CrimeAdapter(mCrimes);
 		setListAdapter(adapter);
+
 	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = super.onCreateView(inflater, container, savedInstanceState);
+
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+			if(mSubtitleVisible){
+				((ActionBarActivity)getActivity()).getSupportActionBar().setSubtitle(R.string.subtitle);
+			}
+		}
+		return v;
+	}
+
 
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
@@ -72,15 +95,20 @@ public class CrimeListFragment extends ListFragment {
 	@Override
 	public void onResume() {
 		super.onResume();
-		((CrimeAdapter)getListAdapter()).notifyDataSetChanged();
+		((CrimeAdapter) getListAdapter()).notifyDataSetChanged();
 	}
 
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		super.onCreateOptionsMenu(menu, inflater);
 		inflater.inflate(R.menu.fragment_crime_list, menu);
+		MenuItem showSubtitle  = menu.findItem(R.id.menu_item_show_subtitle);
+		if(mSubtitleVisible && showSubtitle != null){
+			showSubtitle.setTitle(R.string.show_subtitle);
+		}
 	}
 
+	@TargetApi(11)
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -88,9 +116,21 @@ public class CrimeListFragment extends ListFragment {
 				Crime crime = new Crime();
 				CrimeLab.get(getActivity()).addCrime(crime);
 				Intent i = new Intent(getActivity(), CrimePagerActivity.class);
-				i.putExtra(CrimeFragment.EXTRA_CRIME_ID,crime.getId());
+				i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
 				startActivityForResult(i, 0);
 				return true;
+			case R.id.menu_item_show_subtitle:
+
+				if (((ActionBarActivity) getActivity()).getSupportActionBar().getSubtitle() == null) {//this is a bunch of bullshit caused by api 8 and not recognized by the book
+					((ActionBarActivity) getActivity()).getSupportActionBar().setSubtitle(R.string.subtitle);
+					item.setTitle(R.string.hide_subtitle);
+					mSubtitleVisible = true;
+				} else {
+					((ActionBarActivity) getActivity()).getSupportActionBar().setSubtitle(null);
+					item.setTitle(R.string.show_subtitle);
+					mSubtitleVisible = false;
+				}
+
 			default:
 				return super.onOptionsItemSelected(item);
 		}
--- app/src/main/java/com/helwigdev/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/helwigdev/criminalintent/SingleFragmentActivity.java
@@ -1,19 +1,24 @@
 package com.helwigdev.criminalintent;
 
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Window;
 
 /**
  * Created by Tyler on 1/15/2015.
  */
-public abstract class SingleFragmentActivity extends ActionBarActivity{
+public abstract class SingleFragmentActivity extends ActionBarActivity {
 	protected abstract Fragment createFragment();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_fragment);
 		FragmentManager fm = getSupportFragmentManager();
 		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
